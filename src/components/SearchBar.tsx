57a2d405ee6d5d56a7648bf88f6bafaa1d16be82
Chris Blakely <chrisblakely01@users.noreply.github.com>
2024-02-02 18:43:58 +0000

    adding search page and filters

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ type Props = {
   onSubmit: (formData: SearchForm) => void;
   placeHolder: string;
   onReset?: () => void;
-  searchQuery: string;
+  searchQuery?: string;
 };
 
 const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
